890859fe891c8e4c562977ac9d2dd0b0c1a9c67a
HNduta <hope.wanjohi@strathmore.edu>
2024-10-12 11:15:13 +0300

    made a few corrections

--- user.php
+++ user.php
@@ -3,9 +3,8 @@ class User {
     private $conn;
     private $table_name = "users";
 
-    public $id;
-    public $firstname;
-    public $lastname;
+    public $id;;
+    public $name;
     public $email;
     public $password;
     public $created_at;
